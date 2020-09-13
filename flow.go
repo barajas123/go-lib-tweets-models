0bb46540547c7225e056dfd77a569a4d268ac68d
Andy <andybarajas0418@gmail.com>
2020-09-13 09:40:16 -0400

    add Flow Response model for api

--- flow.go
+++ flow.go
@@ -15,3 +15,16 @@ type Flow struct {
 	IsOpening  bool    `json:"isOpening"`
 	RefVolume  float32 `json:"refVolume"`
 }
+
+type FlowResponse struct {
+	Ticker     string  `json:"ticker"`
+	Strike     float32 `json:"strike"`
+	PC         string  `json:"pc"`
+	Quantity   int     `json:"quantity"`
+	Price      float32 `json:"price"`
+	Expiration string  `json:"expiration"`
+	BidAsk     string  `json:"bidAsk"`
+	TimeStamp  string  `json:"createdAt"`
+	IsSweep    bool    `json:"isSweep"`
+	IsOpening  bool    `json:"isOpening"`
+}
