841a1c9317826dbae277394c3990fbbd6248e227
Andy Barajas <46165581+barajas123@users.noreply.github.com>
2020-11-02 16:28:52 -0500

    Update flowRaw.go

--- flowRaw.go
+++ flowRaw.go
@@ -1,7 +1,7 @@
 package tweetModels
 
 type FlowRaw struct {
-	TwId      string `json="twid"`
+	TwId      string `json:"twid"`
 	Text      string `json:"text"`
 	TimeStamp string `json:"createdAt"`
 }
